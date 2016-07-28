ba9909ae2a238fac5985839b076b18ffdc1d936a
Albert Ducin <albert.ducin@gmail.com>
2016-07-28 12:37:31 +0200

    --

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 angular.module("ZappApp", [])
 
-.constant("apiUrl", "http://localhost:3000/")
+//.constant("apiUrl", "http://localhost:3000/")
+.constant("apiUrl", "http://localhost/cms_spa/web/app_dev.php/")
 
 .controller("NewsController", ["$scope", "$http", "apiUrl", function($scope, $http, apiUrl){
 
-	$http.get(apiUrl + 'news/')
+	//$http.get(apiUrl + 'news/')
+	$http.get(apiUrl + 'products?search=sprzeg')
 	.then(function(response){
 		$scope.news = response.data;
 	});
+	
+	$scope.UpdateData = function () {
+	    var url = apiUrl + 'products/' + $scope.productId + '/' + $scope.productAttribute;
+	    var config = 'contenttype';
+            var data = $.param({
+		db: $scope.productDb,
+                quantity: $scope.productQuantity
+            });
+
+            $http.put(url, data, config)
+            .then(function (data, status, headers) {
+                $scope.ServerResponse = data.data;
+		console.log($scope.ServerResponse);
+            })
+        };
 
 }]);
